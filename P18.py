2f759672a299c15a59a59b39fd63f254b7c214c8
Sevv0sSnagRolls <douglasking256@gmail.com>
2022-06-07 14:46:08 +1000

    P369

---
+++ P18.py
@@ -0,0 +1,49 @@
+#template file
+
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
+#import scipy
+
+testTree = [ [3],
+             [7, 4],
+             [2, 4, 6],
+             [8, 5, 9, 3] ]
+
+
+def funct(n):
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- P718.py
+++ P718.py
@@ -5,19 +5,9 @@
 #import pandas as pd
 #import scipy
 
-import itertools
-
-#deck = ??representation
-deck =
-
-def isBadugi(subset):
-    #
-    if ():
-        pass
-    return False
-
 def funct(n):
-    subsets = itertools.combinations(deck, n)
+
+    for i in range(0, n)
     return
 
 def BruteForceSolution():
