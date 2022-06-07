c3e4b71a80a91743e79d7b4a4cf96b81a9865c04
Sevv0sSnagRolls <douglasking256@gmail.com>
2022-06-07 11:12:08 +1000

    initialise

---
+++ P369.py
@@ -0,0 +1,41 @@
+#template file
+
+#import matplotlib.pyplot as plt
+#import numpy as np
+#import pandas as pd
+#import scipy
+
+x = [1,2,3,4]
+y = [5,6,7,8]
+plt.plot(x,y)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
